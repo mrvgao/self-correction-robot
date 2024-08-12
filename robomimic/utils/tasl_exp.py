8c6e9fdd5234fd6146c414aa09d9a5ab3102e523
mrvgao <mgao40@jhu.edu>
2024-08-12 16:28:14 -0700

    try to solve  number of dimensions in the tensor input does not match the length of the desired ordering of dimensions i.e. input.dim() = 4 is not equal to len(dims) = 5

--- robomimic/utils/tasl_exp.py
+++ robomimic/utils/tasl_exp.py
@@ -21,10 +21,10 @@ def concatenate_images(batch, direct_obs=False):
     # Concatenate the images along the width dimension (dim=3)
     concatenated_images = torch.cat((left_image, eye_in_hand_image, right_image), dim=-1)
 
-    # if not direct_obs:
-    concatenated_images = concatenated_images.permute(0, 1, 3, 4, 2)
-    # else:
-    #     concatenated_images = concatenated_images.permute(0, 2, 3, 1)
+    if not direct_obs:
+        concatenated_images = concatenated_images.permute(0, 1, 3, 4, 2)
+    else:
+        concatenated_images = concatenated_images.permute(0, 2, 3, 1)
 
     # Add the new concatenated image tensor to the 'obs' dictionary
     batch['obs']['concatenated_images'] = concatenated_images
