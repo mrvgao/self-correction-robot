2e1851672882c0f10016c14aba128a26a28bb377
mrvgao <mgao40@jh.edu>
2024-09-08 02:29:47 +0000

    change num_worker to 2

--- robomimic/tasl_exp/reward_model.py
+++ robomimic/tasl_exp/reward_model.py
@@ -126,8 +126,8 @@ def main(args):
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10)
-    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=10)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
 
     # Create the dataloader
     # Example training loop
