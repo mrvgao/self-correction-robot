f048b11c9c49c91c5a92b1faa054ee885d46fd0e
mrvgao <mgao40@jh.edu>
2024-09-09 19:09:52 +0000

    add bash running in tasl

--- running_in_tasl.bash
+++ running_in_tasl.bash
@@ -4,3 +4,5 @@
 /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/tasl_xfmr_value_loss_lambda_5e-1_multi-task-with-value-correct-seed-999.json > tasl-logs/vloss-lambda-5e-1.log &
 /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/tasl_xfmr_multi-task-with-value-correct-seed-999.json > tasl-logs/vloss-lambda-5.log &
 /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/values_loss_lambda_1e1-tasl_xfmr_multi-task-with-value-correct-seed-999.json > tasl-logs/vloss-lambda-1.log &
+
+wait
