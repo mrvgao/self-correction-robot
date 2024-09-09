a24285d5dfd6561097954f1b2972464b45281c5e
mrvgao <mgao40@jh.edu>
2024-09-09 19:15:12 +0000

    add bash running in tasl

--- running_in_tasl.bash
+++ running_in_tasl.bash
@@ -4,5 +4,6 @@
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/tasl_xfmr_value_loss_lambda_5e-1_multi-task-with-value-correct-seed-999.json > tasl-logs/vloss-lambda-5e-1.log &
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/tasl_xfmr_multi-task-with-value-correct-seed-999.json > tasl-logs/vloss-lambda-5.log &
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/values_loss_lambda_1e1-tasl_xfmr_multi-task-with-value-correct-seed-999.json > tasl-logs/vloss-lambda-1.log &
+nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/values_loss_lambda_1e-1-tasl_xfmr_multi-task-with-value-correct-seed-999.json> tasl-logs/vloss-lambda-1e-1.log &
 
 wait
