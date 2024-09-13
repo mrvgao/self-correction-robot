aeff6d192f7b6c7f410ba873b8af2e5e8ad1ce67
mrvgao <mgao40@jh.edu>
2024-09-13 01:48:11 +0000

    add rollout vloss threshold paramter

--- running_in_tasl.bash
+++ running_in_tasl.bash
@@ -7,6 +7,7 @@ nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/lambda_5.json > tasl-logs/lambda_5.log &
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/lambda_50.json > tasl-logs/lambda_50.log &
 nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/lambda_5.json > tasl-logs/lambda_5.log &
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/lambda_5_50_demos.json > tasl-logs/lambda_5_50_demos.log &
 
 nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_4e-2.json > tasl-logs/rollout_lambda_5_1k_vloss_4e.log &
 nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_2e-2.json > tasl-logs/rollout_lambda_5_600_vloss_2e.log &
