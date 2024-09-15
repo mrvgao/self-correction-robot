dc22b38f3358c5679e7d85c96c8190404e9bf95d
mrvgao <mgao40@jh.edu>
2024-09-15 16:18:48 +0000

    add rollout vloss threshold paramter

--- running_in_tasl.bash
+++ running_in_tasl.bash
@@ -6,12 +6,21 @@ nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/lambda-1-point-5.json > tasl-logs/lambda-1-point-5.log &
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/lambda_5.json > tasl-logs/lambda_5.log &
 nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/lambda_50.json > tasl-logs/lambda_50.log &
+nohup /home/minquangao/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/tasl/tasl_lambda_5_50_demos.json > tasl-logs/lambda_50_human_50.log &
+
 nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/lambda_5.json > tasl-logs/lambda_5.log &
 nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/lambda_5_50_demos.json > tasl-logs/lambda_5_50_demos.log &
 
 nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_4e-2.json > tasl-logs/rollout_lambda_5_1k_vloss_4e.log &
-nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_2e-2.json > tasl-logs/rollout_lambda_5_600_vloss_2e.log &
-nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_epoch_545_vloss_2e-2.json > tasl-logs/rollout_lambda_5_epoch_545_vloss_2e.log &
-nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_epoch_545_vloss_4e-2.json > tasl-logs/lambdrollout_lambda_5_epoch_545_vloss_4e.log &
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_4e-2-running-2.json > tasl-logs/rollout_lambda_5_1k_vloss_4e_running_2.log &
+
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_2e-2.json > tasl-logs/rollout_lambda_5_1k_vloss_2e.log &
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_1k_vloss_2e-2-running-2.json > tasl-logs/rollout_lambda_5_1k_vloss_2e_running_2.log &
+
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_450_vloss_2e-2.json > tasl-logs/rollout_lambda_5_epoch_450_vloss_2e.log &
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_450_vloss_2e-2-running-2.json > tasl-logs/rollout_lambda_5_epoch_450_vloss_2e_running_2.log &
+
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_450_vloss_4e-2.json > tasl-logs/lambdrollout_lambda_5_epoch_450_vloss_4e.log &
+nohup /home/ubuntu/anaconda3/envs/robocasa/bin/python -u self_correct_robot/scripts/train.py --config=self_correct_robot/scripts/running_configs/LServer/rollout_lambda_5_450_vloss_4e-2-running-2.json > tasl-logs/lambdrollout_lambda_5_epoch_450_vloss_4e_running_2.log &
 
 wait
