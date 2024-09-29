c6ef06e2b009d1b5e5f2dfa9a7da7268ba6945cc
mrvgao <mgao40@jh.edu>
2024-09-29 22:19:54 +0000

    change rollout use target model

--- self_correct_robot/tasl_exp/rollout.py
+++ self_correct_robot/tasl_exp/rollout.py
@@ -159,7 +159,7 @@ def run_rollout(
             # original_ac_dist, execute_ac, execute_value_predict = get_deployment_action_and_value_from_obs(
             #     rollout_policy=policy, obs_dict=ob_dict)
             find, ploss = find_reliable_action(step_i, ob_dict, env, policy, config, video_frames, progress_bar, previous_p_loss)
-            previous_p_loss = ploss
+            previous_p_loss = ploss.detach().clone()
             plosses.append(ploss.cpu().detach().numpy())
 
         ac = policy(ob=ob_dict, goal=goal_dict)
