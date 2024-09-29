306b5790d7ecb68c0143461bef82d334cd3eb187
mrvgao <mgao40@jh.edu>
2024-09-29 19:22:20 +0000

    change rollout use target model

--- self_correct_robot/tasl_exp/rollout.py
+++ self_correct_robot/tasl_exp/rollout.py
@@ -111,7 +111,7 @@ def run_rollout(
     assert isinstance(env, EnvBase) or isinstance(env, EnvWrapper) or isinstance(env, SubprocVectorEnv)
 
     import pdb; pdb.set_trace()
-    
+
     batched = isinstance(env, SubprocVectorEnv)
 
     ob_dict = env.reset()
