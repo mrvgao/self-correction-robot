5cf7a6a80d57820d3af3e1e4ecd601e2a0f1749d
mrvgao <minchiuan@zju.edu.cn>
2024-10-02 20:49:19 -0400

    recovery MIMO_Transformer to original add progress emb size in algo config

--- self_correct_robot/algo/algo.py
+++ self_correct_robot/algo/algo.py
@@ -192,7 +192,6 @@ class Algo(object):
             # only make optimizers for networks that have been created - @optim_params may have more
             # settings for unused networks
             if k in self.nets:
-                import pdb; pdb.set_trace()
                 if isinstance(self.nets[k], nn.ModuleList):
                     self.optimizers[k] = [
                         TorchUtils.optimizer_from_optim_params(
