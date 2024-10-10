1f5339b07a7558285967382d0d9370a099f27be8
mrvgao <minchiuan@zju.edu.cn>
2024-10-10 10:54:24 -0400

    change export mechanism for export demo for training progress provider

--- self_correct_robot/tasl_exp/export_all_demo_images_with_concate.py
+++ self_correct_robot/tasl_exp/export_all_demo_images_with_concate.py
@@ -78,178 +78,54 @@ def extract_and_export_image(demo_dataset, task_name):
 
     exporting_dataset = demo_dataset
 
-    eye_names = ['robot0_agentview_left_image', 'robot0_eye_in_hand_image', 'robot0_agentview_right_image']
 
     # dir_name = f'/home/ubuntu/robocasa-statics/export-images-from-demo/{_tmp_task_name}'
     # dir_name = f'/home/ubuntu/robocasa-statics/export-multi-tasks/{_tmp_task_name}'
 
     task_name = '_'.join(task_name.split())
 
-    dir_name = f'/home/ubuntu/robocasa-statics/export-images-from-demo-3k/{task_name}'
+    dir_name_left = f'/home/ubuntu/robocasa-statics/export-images-from-demo-3k/{task_name}/left_images/'
+    dir_name_right = f'/home/ubuntu/robocasa-statics/export-images-from-demo-3k/{task_name}/right_images/'
+    dir_name_hand = f'/home/ubuntu/robocasa-statics/export-images-from-demo-3k/{task_name}/hand_images/'
+    dir_name_task_emb = f'/home/ubuntu/robocasa-statics/export-images-from-demo-3k/{task_name}/task_emb/'
 
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
+    dirs_need_to_create = [dir_name_left, dir_name_right, dir_name_hand, dir_name_task_emb]
 
-    PNG_ID = 1
-    TASK_ID = 0
+    for d in dirs_need_to_create:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
-    def write_image_with_name(image1, image2, image3, task_ds_dir, png_id):
-        task_dir = os.path.join(dir_name, task_ds_dir)
+    def write_image_with_name(image, dir_name, step, complete_rate):
+        image_path = os.path.join(dir_name, f'{step}_{task_name}_{complete_rate}.png')
 
         if not os.path.exists(task_dir): os.mkdir(task_dir)
 
-        whole_image = combine_images_horizen([image1, image2, image3])
-        whole_image = np.array(whole_image)
-        whole_image = whole_image.astype(np.uint8)
-        whole_image = cv2.cvtColor(whole_image, cv2.COLOR_BGR2RGB)
-        image_path = os.path.join(task_dir, f'{png_id}.png')
-        cv2.imwrite(image_path, whole_image)
+        # whole_image = combine_images_horizen([image1, image2, image3])
+        image = np.array(image) * 255
+        image = image.astype(np.uint8)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(image_path, image)
 
-    def write_several_images(images1, images2, image3, task_ds_dir, start_id):
-        for l, h, r in zip(images1, images2, image3):
-            write_image_with_name(l, h, r, task_ds_dir, start_id)
-            start_id += 1
+    def write_task_emb_with_name(task_emb, dir_name, step, complete_rate):
+        task_emb_path = os.path.join(dir_name, f'{task_desp}.npy')
+        np.save(task_emb_path, task_emb)
 
-    # MAX_TRY = 10
-    previous_gripper_equal = False
-
-    # task_length = 0
-
-    current_task_string = ''
-
-    number_ds_map = {}
-
-    # Function to extract gripper_db from a single dataset entry
-
-    # start_time = time.time()
-    # gripper_db_mapping = prefetch_gripper_db(exporting_dataset)  # Precompute and save the mapping
-    # time_gripper_db = time.time() - start_time
-    # print(f'Time for parallized gripper_db assignment at iteration {time_gripper_db:.6f} seconds')
-
-    current_demo_id = None
-    previous_demo_id = None
-
-    index_to_demo_id = exporting_dataset._index_to_demo_id
+    eye_names = ['robot0_agentview_left_image', 'robot0_eye_in_hand_image', 'robot0_agentview_right_image']
 
     for i in tqdm(range(len(exporting_dataset))):
-        start_time = time.time()
+        import pdb; pdb.set_trace()
+        left_image = exporting_dataset[i]['obs'][eye_names[0]]
+        hand_image = exporting_dataset[i]['obs'][eye_names[1]]
+        right_image = exporting_dataset[i]['obs'][eye_names[2]]
+        task_emb = exporting_dataset[i]['task_emb']
+        complete_rate = exporting_dataset[i]['progress']
 
-        # Measure the time for gripper_db assignment
-        # gripper_db = exporting_dataset[i]['obs']['robot0_gripper_qpos']
-        # gripper_db = gripper_db_mapping[i]
-        # time_gripper_db = time.time() - start_time
-        # print(f'Time for gripper_db assignment at iteration {i}: {time_gripper_db:.6f} seconds')
+        # get three images
+        # get task embedding
+        # save three images into three folders, left_image, hand_image, right_image
+        # each_file_name_will be '{task}_{i}_{complete_rate}.png'
+        # save the task embedding into a folder, and the file named '{task}_{i}_{complete_rate}.npy'
 
-        start_time = time.time()
-        current_demo_id = index_to_demo_id[i]
-
-        time_set_task_id = time.time() - start_time
-        print(f'Time for GETTING DEMO ID {i}: {time_set_task_id:.6f} seconds')
-
-        if previous_demo_id is None:
-            previous_demo_id = current_task_string
-
-        # start_time = time.time()
-        # batch_size = len(gripper_db)
-        # time_batch_size = time.time() - start_time
-        # print(f'Time for batch_size calculation at iteration {i}: {time_batch_size:.6f} seconds')
-
-        # start_time = time.time()
-        # gripper_equal = (gripper_db[-1] == gripper_db[-2]).all()
-        # time_delta = time.time() - start_time
-        # print(f'Time for delta calculation at iteration {i}: {time_delta:.6f} seconds')
-
-        # start_time = time.time()
-        # change_task = previous_demo_id
-        # time_change_task = time.time() - start_time
-        # print(f'Time for change_task check at iteration {i}: {time_change_task:.6f} seconds')
-
-        start_time = time.time()
-        batch_size = 19
-
-        task_changed = previous_demo_id != current_demo_id or i == 0
-
-        if task_changed:
-            PNG_ID += batch_size
-            exporting_dataset.set_progress(i, np.array([p for p in range(batch_size)]))
-        else:
-            PNG_ID += 1
-            exporting_dataset.set_progress(i, np.array([PNG_ID - di for di in reversed(range(batch_size))]))
-
-        time_set_progress = time.time() - start_time
-        # print(f'Time for progress setting at iteration {i}: {time_set_progress:.6f} seconds')
-
-        start_time = time.time()
-
-        # if task_changed:
-        #     mark_position = i
-        #     while mark_position >= 0 and exporting_dataset[mark_position]['task_id'] == TASK_ID:
-        #         ds_progress = exporting_dataset[mark_position]['progress']
-        #         exporting_dataset.set_progress(mark_position, ds_progress / PNG_ID * 100)
-        #         mark_position -= 1
-        #     print(f'task name: {task_name}')
-        #     PNG_ID = 0
-
-        time_task_change = time.time() - start_time
-        # print(f'Time for task change block at iteration {i}: {time_task_change:.6f} seconds')
-
-        # start_time = time.time()
-        # previous_gripper_equal = gripper_equal
-        previous_demo_id = current_demo_id
-        # time_previous_delta = time.time() - start_time
-        # print(f'Time for updating previous_delta at iteration {i}: {time_previous_delta:.6f} seconds')
-
-
-    """
-
-    for i in tqdm(range(len(exporting_dataset))):
-        gripper_db = exporting_dataset[i]['obs']['robot0_gripper_qpos']
-
-        exporting_dataset.set_task_id(i, TASK_ID)
-
-        batch_size = len(gripper_db)
-
-        # exporting_dataset[i]['TEST_ID'] = TASK_ID
-        # number_ds_map{i} = TASK_ID
-        # exporting_dataset[i]['obs']['progress'] = np.zeros(len(gripper_db))
-
-        delta = np.mean(gripper_db[-1] - gripper_db[-2])
-
-        change_task = (previous_delta == 0 and delta != 0)
-
-        # i == 0 [0, 1, 2, 3, 4]
-        # i == 1 [1, 2, 3, 4, 5]
-        # 5 - 5
-
-        if i == 0:
-            PNG_ID += batch_size
-            exporting_dataset.set_progress(i, np.array([p for p in range(batch_size)]))
-        else:
-            PNG_ID += 1
-            exporting_dataset.set_progress(i, np.array([PNG_ID - di for di in reversed(range(batch_size))]))
-
-        if change_task:
-
-            mark_position = i
-            # track back
-            while mark_position >= 0 and exporting_dataset[mark_position]['task_id'] == TASK_ID:
-                ds_progress = exporting_dataset[mark_position]['progress']
-                exporting_dataset.set_progress(mark_position, ds_progress / PNG_ID * 100)
-                mark_position -= 1
-
-            print(f'task name: {task_name}')
-
-            TASK_ID += 1
-
-        previous_delta = delta
-        #
-        # if i == 0:
-        #     write_several_images(left_db, hand_db, right_db, task_ds_dir, start_id=1)
-        #     PNG_ID += len(left_db)
-        # else:
-        #     write_image_with_name(left_db[-1], right_db[-1], hand_db[-1], task_ds_dir, png_id=PNG_ID)
-        #     PNG_ID += 1
-    """
 
 
 def generate_concated_images_from_demo_path(task_name):
