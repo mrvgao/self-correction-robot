a5155e74ea2cbe6daa008920db9d35f057b6ad68
mrvgao <minchiuan@zju.edu.cn>
2024-10-20 12:40:08 -0400

    update value model train

--- self_correct_robot/tasl_exp/reward_model.py
+++ self_correct_robot/tasl_exp/reward_model.py
@@ -21,7 +21,6 @@ import time
 import hashlib
 from self_correct_robot.utils.load_dataloader import load_dataloader
 import json
-from self_correct_robot.config import config_factory
 
 torch.backends.cudnn.benchmark = True
 
@@ -40,188 +39,116 @@ def set_seed(seed):
     # torch.backends.cudnn.benchmark = False
 
 
-def get_subset_dataloader(dataloader, percentage):
-    # Get the dataset from the DataLoader
-    dataset = dataloader.dataset
-    dataset_size = len(dataset)
-
-    # Calculate subset size (20% in your case)
-    subset_size = int(percentage * dataset_size)
-
-    # Get a list of all indices from the dataset
-    all_indices = list(range(dataset_size))
-
-    # Shuffle the indices
-    random.shuffle(all_indices)
-
-    # Select 20% of the shuffled indices
-    subset_indices = all_indices[:subset_size]
-
-    # Use Subset to create a new dataset with the selected indices
-    subset = Subset(dataset, subset_indices)
-
-    # Create a new DataLoader with the subset
-    subset_dataloader = DataLoader(subset, batch_size=dataloader.batch_size, shuffle=False,
-                                   num_workers=dataloader.num_workers)
-
-    return subset_dataloader
-
-
-class CustomImageDataset(Dataset):
-    def __init__(self, root_dir, feature_extractor, device, target_task=None):
-        self.root_dir = root_dir
-        self.feature_extractor = feature_extractor
-        self.image_pairs = []
-        self.lang_encoder = LangEncoder(device=device)
-        self.target_task = target_task
-        self.tasks = []
-        self.task_embeddings = []
-
-        # Prepare a list of all image pairs and corresponding labels
-        for ti, task_name_dir in enumerate(os.listdir(root_dir)):
-            if target_task is not None and task_name_dir != target_task: continue
-
-            for task_ds_str in os.listdir(os.path.join(root_dir, task_name_dir)):
-
-                task_path = os.path.join(root_dir, task_name_dir, task_ds_str) # export/prepare-coffee/1
-
-                rm_index = task_ds_str.find('_ID_')
-                task_ds_str = task_ds_str[:rm_index]
-
-                current_task_name = ' '.join(task_ds_str.split('_'))
-                print('init task: ', current_task_name)
-                if os.path.isdir(task_path):
-                    images = sorted([f for f in os.listdir(task_path) if f.endswith('.png')])
-                    # Get the numeric parts of the filenames
-                    image_numbers = sorted([int(os.path.splitext(f)[0]) for f in images])
-
-                    N = image_numbers[-1]
-
-                    for i in range(len(image_numbers)):
-                        image_path = os.path.join(task_path, f'{image_numbers[i]}.png')
-
-                        # Calculate the label
-                        # label = -1 * (N - image_numbers[i]) + 1
-                        label = image_numbers[i] / N
-                        self.image_pairs.append((image_path, label))
-                        self.tasks.append(current_task_name)
-
-        self._batch_process_tasks()
-
-    def _batch_process_tasks(self):
-        """
-        Batch process task embeddings for all task names.
-        """
-        print("Batch processing task embeddings...")
-        feature_batch_size = 1024
-        for i in tqdm(range(0, len(self.tasks), feature_batch_size)):
-            batch_task_names = self.tasks[i:i + feature_batch_size]
-            task_embeddings = self.lang_encoder.get_lang_emb(batch_task_names)
-            self.task_embeddings.extend(task_embeddings)
-
-    def __len__(self):
-        return len(self.image_pairs)
-
-    def __getitem__(self, idx):
-        image_path, label = self.image_pairs[idx]
-
-        # Load the images
-        image = Image.open(image_path).convert('RGB')
-        image = self.feature_extractor(image)
-
-        task_embedding = self.task_embeddings[idx]
-
-        return image, task_embedding, torch.tensor(label, dtype=torch.float32)
-
-
-class NumpyToTensor:
-    """Custom transform to convert a NumPy array directly to a PyTorch tensor."""
-    def __call__(self, img):
-        if isinstance(img, np.ndarray):
-            # Convert NumPy array (H x W x C) to PyTorch tensor (C x H x W)
-            img = torch.from_numpy(img).permute(2, 0, 1).float() / 255.0
-        return img
-
-
-resnet_transformer = transforms.Compose([
-    NumpyToTensor(),  # C
-    transforms.Resize((224, 224), antialias=True),
-    # transforms.ToTensor(),
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transformer = transform
+        self.lang_embedding = dict()
+        self.data = []
 
-# Custom Dataset that applies the transformer
-class RoboCustomDataset(Dataset):
-    def __init__(self, dataset, transformer):
-        self.dataset = dataset  # Original dataset from the DataLoader
-        self.transformer = transformer
+        for sub_task in os.listdir(root_dir):
+            sub_task_dir = os.path.join(root_dir, sub_task)
+
+            for task_desc in os.listdir(os.path.join(sub_task_dir, 'task_emb')):
+                task_emb_path = os.path.join(sub_task_dir, 'task_emb', task_desc)
+                if not os.path.isfile(task_emb_path):
+                    print(f"Task embedding not found for {task_emb_path}, skipping.")
+                    continue
+                else:
+                    task_desc_name = task_desc.split('.')[0]
+                    self.lang_embedding[task_desc_name] = np.load(task_emb_path)
+
+            image_dirs = ['left_images', 'hand_images', 'right_images']
+
+            image_paths = {img_dir: [] for img_dir in image_dirs}
+
+            for img_dir in image_dirs:
+                for img_name in os.listdir(os.path.join(sub_task_dir, img_dir)):
+                    img_path = os.path.join(sub_task_dir, img_dir, img_name)
+                    if not os.path.isdir(img_path):
+                        continue
+
+                    for img_file in os.listdir(img_path):
+                        img_file_path = os.path.join(img_path, img_file)
+                        tokens = img_file.rsplit('_')
+                        label = float(tokens[-1].replace('.png', ''))
+                        task_name = '_'.join(tokens[1:-1])
+                        image_paths[img_dir].append((img_file_path, task_name, label))
+
+            num_images = min(len(image_paths[img_dir]) for img_dir in image_dirs)
+
+            for i in range(num_images):
+                img_1_path, task_name, label = image_paths['left_images'][i]
+                img_2_path, task_name_1, label_1 = image_paths['hand_images'][i]
+                img_3_path, task_name_2, label_2 = image_paths['right_images'][i]
+
+                assert task_name == task_name_1 == task_name_2
+                assert label == label_1 == label_2
+
+                self.data.append((img_1_path, img_2_path, img_3_path, task_name, label))
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        # Assuming the dataset is structured as dataset[idx]['obs']
-        # n = 100
+        left_image, hand_image, right_image, task_name, label = self.data[idx]
 
-        # t = time.time()
-        # for _ in range(n):
-        # obs_data = self.dataset.dataset.get_progress_train(idx)
-        # print('get progress new train used time, ', time.time() - t)
+        # Load images
+        left_image = Image.open(left_image).convert('RGB')
+        hand_image = Image.open(hand_image).convert('RGB')
+        right_image = Image.open(right_image).convert('RGB')
 
-        # t = time.time()
-        # for _ in range(n):
-        #     obs_data = self.dataset[idx]
+        if self.transform:
+            left_image = self.transform(left_image)
+            hand_image = self.transform(hand_image)
+            right_image = self.transform(right_image)
 
-        # print('get progress old train used time, ', time.time() - t)
+        task_emb = self.lang_embedding[task_name]
+        task_emb = torch.tensor(task_emb, dtype=torch.float32)
 
-        obs_data = self.dataset[idx]
+        label = torch.tensor(label, dtype=torch.float32)
 
-        progress_label = torch.tensor(obs_data['progress'], dtype=torch.float32)
-        left_image = obs_data['obs']['robot0_agentview_left_image'][0]
-        hand_image = obs_data['obs']['robot0_eye_in_hand_image'][0]
-        right_image = obs_data['obs']['robot0_agentview_right_image'][0]
-        task_emb = torch.tensor(obs_data['obs']['lang_emb'][0], dtype=torch.float32)
+        import pdb; pdb.set_trace()
 
-        # Extract the three necessary features, you can customize these keys
-        # Apply the resnet_transformer to each feature
-        feature_left = self.transformer(left_image)
-        feature_hand = self.transformer(hand_image)
-        feature_right = self.transformer(right_image)
-
-        return feature_left, feature_hand, feature_right, task_emb, progress_label
+        return left_image, hand_image, right_image, task_emb, label
 
 
-def split_valid_test_from_robo_config_dataset(config, batch_size):
+def create_dataloaders(my_dataset, batch_size, train_split=0.7, val_split=0.15, test_split=0.15):
+    """
+    Create train, validation, and test dataloaders from a dataset.
 
-    my_dataloader = load_dataloader(config, device='cuda:0')[0]
+    Args:
+        dataset (Dataset): The dataset to split.
+        batch_size (int): The batch size for the dataloaders.
+        train_split (float): Fraction of the dataset to use for training.
+        val_split (float): Fraction of the dataset to use for validation.
+        test_split (float): Fraction of the dataset to use for testing.
 
-    dataset = my_dataloader.dataset  # Retrieve the dataset from the existing DataLoader
+    Returns:
+        train_loader, val_loader, test_loader: DataLoader for each split.
+    """
+    # Ensure the splits sum to 1
+    assert train_split + val_split + test_split == 1, "Splits must sum to 1."
 
-    # Step 1: Define the lengths for train, validation, and test splits
-    dataset_size = len(dataset)
+    # Calculate the number of samples in each split
+    total_size = len(my_dataset)
+    train_size = int(train_split * total_size)
+    val_size = int(val_split * total_size)
+    test_size = total_size - train_size - val_size  # Ensure all samples are used
 
-    train_size = int(0.85 * dataset_size)  # 70% training
-    val_size = int(0.15 * dataset_size)  # 15% validation
-    test_size = dataset_size - train_size - val_size  # Remaining for test
+    # Split the dataset
+    train_dataset, val_dataset, test_dataset = random_split(my_dataset, [train_size, val_size, test_size])
 
-    # Step 2: Split the dataset into train, validation, and test
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
-
-    # Step 3: Wrap the datasets into the custom dataset class that applies the transformation
-    train_dataset = RoboCustomDataset(train_dataset, resnet_transformer)
-    val_dataset = RoboCustomDataset(val_dataset, resnet_transformer)
-    test_dataset = RoboCustomDataset(test_dataset, resnet_transformer)
-
-    # Step 4: Create new DataLoaders for each split
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=24, pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=24, pin_memory=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=24, pin_memory=True)
-
-    # percentage = 0.2
-    # train_loader = get_subset_dataloader(train_loader, percentage)
-    # val_loader = get_subset_dataloader(val_loader, percentage)
-    # test_loader = get_subset_dataloader(test_loader, percentage)
+    # Create DataLoader for each split
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader, test_loader
 
@@ -232,8 +159,7 @@ def main(args):
     device = torch.device(f'cuda:{args.cuda}')
     print(f"Using device: {device}")
 
-    train_dataloader_whole, val_dataloader_whole, test_dataloader = split_valid_test_from_robo_config_dataset(args.config,
-                                                                                                  batch_size=args.batch_size)
+    train_dataloader, val_dataloader, test_dataloader = create_dataloaders('')
 
     # Create the dataloader
     # Example training loop
@@ -270,18 +196,13 @@ def main(args):
     early_stopping_counter = 0
 
     test_dataloader_iter = iter(test_dataloader)
+    train_dataloader_iter = iter(train_dataloader)
+    val_dataloader_iter = iter(val_dataloader)
 
     for epoch in range(num_epochs):
-        percentage = 0.2
-        train_dataloader = get_subset_dataloader(train_dataloader_whole, percentage=percentage)
-        val_dataloader = get_subset_dataloader(val_dataloader_whole, percentage=percentage)
-
-        train_dataloader_iter = iter(train_dataloader)
-        val_dataloader_iter = iter(val_dataloader)
-
         model.train()
         running_loss = 0.0
-        progress_bar = tqdm(range(len(train_dataloader_whole)), desc=f"Epoch {epoch + 1}/{num_epochs} [Training]", leave=True)
+        progress_bar = tqdm(range(len(train_dataloader)), desc=f"Epoch {epoch + 1}/{num_epochs} [Training]", leave=True)
         batch_loss = 0
 
         for i in progress_bar:
@@ -316,7 +237,7 @@ def main(args):
 
             progress_bar.set_postfix(loss=loss.item())
 
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Training Loss: {batch_loss / len(train_dataloader_whole):.4f}")
+        print(f"Epoch [{epoch + 1}/{num_epochs}], Training Loss: {batch_loss / len(train_dataloader):.4f}")
 
         # Save the model
 
@@ -324,7 +245,7 @@ def main(args):
             model.eval()
             val_loss = 0.0
             with torch.no_grad():
-                progress_bar = tqdm(range(len(val_dataloader_whole)), desc=f"Epoch {epoch + 1}/{num_epochs} [Validation]", leave=True)
+                progress_bar = tqdm(range(len(val_dataloader)), desc=f"Epoch {epoch + 1}/{num_epochs} [Validation]", leave=True)
 
                 for _ in progress_bar:
                     try:
@@ -343,7 +264,7 @@ def main(args):
                     loss = criterion(outputs, labels.unsqueeze(1))
                     val_loss += loss.item()
 
-                avg_val_loss = val_loss / len(val_dataloader_whole)
+                avg_val_loss = val_loss / len(val_dataloader)
                 current_lr = optimizer.param_groups[0]['lr']
                 wandb.log({"epoch": epoch + 1, "val_loss": avg_val_loss, 'lr': current_lr})
                 print(f"Epoch [{epoch + 1}/{num_epochs}], Validation Loss: {avg_val_loss:.4f}")
@@ -410,32 +331,12 @@ if __name__ == "__main__":
     device = torch.device('cuda')  # This will refer to the first available GPU in your restricted list
 
     parser = argparse.ArgumentParser(description='Train a Value Predication Model Via Vision Transformer model.')
-    parser.add_argument('--config', type=str, required=True, help='give the data path config')
     parser.add_argument('--tag', type=str, required=True, help='Add a tag to make logger easier')
     parser.add_argument('--model', type=str, required=False, choices=['attn', 'resnet'], help='Name for the selection model')
     parser.add_argument('--seed', type=int, required=True, help='training random seed')
     parser.add_argument('--ckpt', type=str, default=None, required=False, help='specify a pretraind model path')
     args = parser.parse_args()
 
-    # config_path = '/home/ubuntu/self-correction-robot/self_correct_robot/scripts/running_configs/lambda_multi-task-with-value-correct-seed-999.json'
-
-    ext_cfg = json.load(open(args.config, 'r'))
-    config = config_factory(ext_cfg["algo_name"])
-    # update config with external json - this will throw errors if
-    # the external config has keys not present in the base algo config
-    with config.values_unlocked():
-        config.update(ext_cfg)
-
-    split_valid_test_from_robo_config_dataset(config=config, batch_size=512)
-    # mp.set_start_method('spawn', force=True)  # Set multiprocessing to use 'spawn'
-    #
-    # parser = argparse.ArgumentParser(description='Train a Value Predication Model Via Vision Transformer model.')
-    # # parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate for the optimizer.')
-    # # parser.add_argument('--batch_size', type=int, default=8, help='Batch size for training.')
-    # # parser.add_argument('--num_epochs', type=int, default=5, help='Number of epochs for training.')
-    # # parser.add_argument('--cuda', type=str, required=True, help='the No of cuda')
-    # # parser.add_argument('--task_dir', type=str, default=None, required=False, help='specify a task')
-    #
     timestamp = str(int(time.time()))
     signature = hashlib.sha256(timestamp.encode()).hexdigest()
 
@@ -451,14 +352,11 @@ if __name__ == "__main__":
     Args = namedtuple(
         'Args',
         ['name', 'model', 'lr',  'batch_size', 'num_epochs',
-                    'cuda', 'seed', 'task_dir', 'config', 'ckpt']
+                    'cuda', 'seed', 'task_dir', 'ckpt']
     )
     # #
     # # for i, task_dir in enumerate(sub_tasks):
-    mock_args = Args(name, model, lr, batch_size, num_epochs, cuda, args.seed, None, config, args.ckpt)
+    mock_args = Args(name, model, lr, batch_size, num_epochs, cuda, args.seed, None, args.ckpt)
     run_name = f"{name}_{model}_lr_{lr}_bs_{batch_size}_epochs_{num_epochs}_seed_{args.seed}"
-    # wandb.init(project="value-model-trained-by-3k", entity="minchiuan", name=run_name, config128
-    #     ""
-    #     "system_metrics": True  # Enable system metrics logging
-    # })
+
     main(mock_args)
